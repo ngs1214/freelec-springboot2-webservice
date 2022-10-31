c61f73ac835adedd7e8e6fa168e82d549325624d
ngs1214 <ngs1214@gmail.com>
2022-10-31 15:33:08 +0900

    무중단배포 스크립트만들기  - profileAPI추가    : SecurityConfig 수정    : CrofileController 추가

--- src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest.java
@@ -1,6 +1,5 @@
 package com.jojoldu.book.springboot.web;
 
-import com.jojoldu.book.springboot.web.dto.ProfileController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
